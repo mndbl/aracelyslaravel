83e55b23dd6f63aeec031171709416825f859f46
José Mendible <66981978+mndbl@users.noreply.github.com>
2021-09-29 18:29:48 -0400

    Corregir nombre

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,5 +1,5 @@
  <footer class="flex flex-col items-center justify-between px-6 py-4 bg-white dark:bg-gray-800 sm:flex-row">
-            <a href="/" class="text-xl font-bold text-gray-800 dark:text-white hover:text-gray-700 dark:hover:text-gray-300">The Total 305 Experience</a>
+            <a href="/" class="text-xl font-bold text-gray-800 dark:text-white hover:text-gray-700 dark:hover:text-gray-300">The 305 Total Experience</a>
 
             <p class="py-2 text-gray-800 dark:text-white sm:py-0">All rights reserved</p>
 
