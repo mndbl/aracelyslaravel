b4587655bb953217708a819e668da516e2e9753a
José Mendible <66981978+mndbl@users.noreply.github.com>
2021-10-01 15:00:58 -0400

    nombre de app

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'The 305 Total Experience'),
 
     /*
     |--------------------------------------------------------------------------
