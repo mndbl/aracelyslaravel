734061de1ed0a8d62749ec8a81cc03f8c789e2ec
José Mendible <66981978+mndbl@users.noreply.github.com>
2021-10-04 14:53:43 -0400

    busines-plan

--- resources/views/pages/business-plan.blade.php
+++ resources/views/pages/business-plan.blade.php
@@ -2,7 +2,7 @@
 @section('pages')
     <div class="py-12 bg-white mx-auto">
   <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-justify">
-    <div class="lg:mx-auto">
+    <div class="lg:mx-auto w-full">
       <h2 class="text-base text-indigo-600 font-semibold tracking-wide uppercase">Business Plan</h2>
       <p class="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
         Executive Summary
@@ -85,7 +85,7 @@ and patents.
       <h2 class="text-base text-gray-600 font-semibold tracking-wide uppercase">
       Business Goals and Objectives
           </h2>
-          <div class="w-full lg:flex">
+          <div class="max-w-2xl lg:flex">
               <div class="lg:w-1/2">
 <p class="font-bold mt-4 max-w-2xl text-md text-gray-500">
       Short Term
