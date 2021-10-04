e061b6a3b02a6626a7363aeb3854db5cabb2c03d
José Mendible <66981978+mndbl@users.noreply.github.com>
2021-10-04 14:44:02 -0400

    ajuste nombre

--- resources/views/pages/business-plan.blade.php
+++ resources/views/pages/business-plan.blade.php
@@ -1,7 +1,7 @@
 @extends('welcome')
 @section('pages')
     <div class="py-12 bg-white mx-auto">
-  <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+  <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-justify">
     <div class="lg:mx-auto">
       <h2 class="text-base text-indigo-600 font-semibold tracking-wide uppercase">Business Plan</h2>
       <p class="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
