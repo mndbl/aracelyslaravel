5eb67cd270cf6b48e9a3f88964953916f04f6f66
José Mendible <66981978+mndbl@users.noreply.github.com>
2021-11-20 04:34:49 -0400

    quitar tesla

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -130,21 +130,7 @@
                         <span class="mt-1 font-medium text-gray-600 dark:text-gray-300">Software Engineer</span> */} --}}
               </div>
             </div>
-            <div class="w-full max-w-xs text-center">
-              <a href="https://www.tesla.com/">
-                <img
-                  class="object-contain object-center w-full h-48 mx-auto rounded-lg"
-                  src="/assets/Tesla.png"
-                  alt="avatar"
-                />
-              </a>
-
-              <div class="mt-2">
-                {{-- {/* <h3 class="text-lg font-medium text-gray-700 dark:text-gray-200">Patterson Johnson</h3>
-                        <span class="mt-1 font-medium text-gray-600 dark:text-gray-300">Software Engineer</span> */} --}}
-              </div>
-            </div>
-          </div>
+        </div>
         </div>
       </section>
       <section class="my-8 mx-2">
